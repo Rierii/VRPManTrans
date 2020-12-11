7336d013389c32cb23d3bb9e620d61e1d0b1655a
Rierii <audreyrizki@gmail.com>
2020-12-11 17:03:04 +0700

    Modul 6 Pertemuan 2

---
+++ CVRP_Skenario_6.py
@@ -0,0 +1,144 @@
+"""SKENARIO 4"""
+# Kapasitas 100, Jarak 55
+
+from __future__ import print_function
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+def create_data_model():
+    """Stores the data for the problem."""
+    data = {}
+    data['distance_matrix'] = [
+        [
+            0, 707,  510, 224, 412, 1342, 707, 566, 800, 224, 100
+        ],
+        [
+            707, 0, 849, 806, 1118, 1304, 1131, 583, 141, 854, 700
+        ],
+        [
+            510, 844, 0, 728, 640, 860, 283, 316, 860, 361, 608
+        ],
+        [
+            224, 806, 728, 0, 424, 1565, 900, 781, 922, 400, 141
+        ],
+        [
+            412, 1118, 640, 424, 0, 1487, 671, 854, 1204, 316, 447
+        ],
+        [
+            1342, 1304, 860, 1556, 1487, 0, 860, 825, 1217, 1221, 1432
+        ],
+        [
+            707, 1131, 283, 900, 671, 860, 0, 583, 1140, 500, 806
+        ],
+        [
+            566, 583, 316, 781, 854, 825, 583, 0, 566, 539, 640
+        ],
+        [
+            800, 141, 860, 922, 1204, 1217, 1140, 566, 0, 922, 806
+        ],
+        [
+            224, 854, 361, 400, 316, 1221, 500, 539, 922, 0, 316
+        ],
+        [
+            100, 700, 608, 141, 447, 1432, 806, 640, 806, 316, 0
+        ]
+    ]
+    data['demands'] = [0, 39, 48, 27, 35, 30, 26, 44, 53, 32, 42]
+    data['vehicle_capacities'] = [100,100,100,100,100]
+    data['num_vehicles'] = 5
+    data['depot'] = 0
+    return data
+
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    max_route_distance = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        while not routing.IsEnd(index):
+            plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        print(plan_output)
+        max_route_distance = max(route_distance, max_route_distance)
+    print('Maximum of the route distances: {}m'.format(max_route_distance))
+
+
+
+
+def main():
+    """Solve the CVRP problem."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(
+        demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        data['vehicle_capacities'],  # vehicle maximum capacities
+        True,  # start cumul to zero
+        'Capacity')
+
+    # Add Distance constraint.
+    dimension_name = 'Distance'
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        3500,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name)
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Setting first solution heuristic wit time limit if solution not found
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.time_limit.seconds = 5
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+if __name__ == '__main__':
+    main()
